811220ebc5dfa0fd733a4f2f0283b2c267aa3763
ARJUN GODARA <120511028+ArjunGodara08@users.noreply.github.com>
2025-09-10 09:50:58 +0530

    Fix Express order and serve React frontend correctly

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
+
 const { initDB } = require('./models');
 const boardRoutes = require('./routes/boards');
 const setupSockets = require('./socket');
-// near top of file
-const path = require('path');
-app.use(express.static(path.join(__dirname, '../public')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 const app = express();
 app.use(cors());
@@ -17,9 +15,16 @@ app.use(express.json());
 // REST routes
 app.use('/api/boards', boardRoutes);
 
-// health
+// health check
 app.get('/health', (req, res) => res.json({ ok: true }));
 
+// Serve React build (after API routes)
+app.use(express.static(path.join(__dirname, '../public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// HTTP + WebSockets
 const server = http.createServer(app);
 const io = setupSockets(server);
 
@@ -35,3 +40,4 @@ initDB()
     console.error('DB init error', err);
     process.exit(1);
   });
+
