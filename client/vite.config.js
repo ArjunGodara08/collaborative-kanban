fae0e1eaf711d0f2e0562858e18d40d63e124ef6
ARJUN GODARA <120511028+ArjunGodara08@users.noreply.github.com>
2025-09-10 09:39:16 +0530

    Fix vite config root for Render build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+iimport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: '.',        // ensure vite sees index.html in client/
   build: {
     outDir: 'dist',
   },
